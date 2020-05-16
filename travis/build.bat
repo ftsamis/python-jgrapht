650e7d2ea3c30fc27a352b14388511e6664b27cb
Fotis Tsamis <ftsamis@gmail.com>
2020-05-16 18:01:22 +0300

    Test Windows bat script.

--- travis/build.bat
+++ travis/build.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 python -VV
 python setup.py bdist_wheel
