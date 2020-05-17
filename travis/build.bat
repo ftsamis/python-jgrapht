d6e01ce49d3867e48b1be3f23d67d7ade41d2b7f
Fotis Tsamis <ftsamis@gmail.com>
2020-05-18 00:48:33 +0300

    Windows builds...

--- travis/build.bat
+++ travis/build.bat
@@ -1,6 +1,7 @@
-set
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-cmake --version
+::set
+::call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+::cmake --version
 ::cmake --help
 ::call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
 ::call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
