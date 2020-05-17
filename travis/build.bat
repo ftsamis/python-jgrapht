51cae84cdaf49de08f5c23a1b05a681e2554d516
Fotis Tsamis <ftsamis@gmail.com>
2020-05-17 16:41:15 +0300

    Set CMAKE_GENERATOR instead of GENERATOR in Windows.

--- travis/build.bat
+++ travis/build.bat
@@ -1,7 +1,5 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+::call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 SET CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
-python -VV
-cl /?
 set
 python setup.py bdist_wheel
 
