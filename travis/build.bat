41bebf60f43b0db426130975e9b76f5eae95f2e5
Fotis Tsamis <ftsamis@gmail.com>
2020-05-17 18:11:42 +0300

    Windows builds...

--- travis/build.bat
+++ travis/build.bat
@@ -1,5 +1,5 @@
 ::call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-SET CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
+SET CMAKE_GENERATOR="Visual Studio 16 2019"
 set
 python setup.py bdist_wheel
 
