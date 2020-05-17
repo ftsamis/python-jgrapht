c94ecbbae07e860d405dcd6909792317f9eb5397
Fotis Tsamis <ftsamis@gmail.com>
2020-05-17 20:41:38 +0300

    Windows builds...

--- travis/build.bat
+++ travis/build.bat
@@ -1,8 +1,11 @@
 ::call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 cmake --version
 cmake --help
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
 
-SET CMAKE_GENERATOR="Visual Studio 16 2019"
+
+::SET CMAKE_GENERATOR="Visual Studio 16 2019"
 set
 python setup.py bdist_wheel
 
