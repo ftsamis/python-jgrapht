fccf9884ce09361de72555df18d672c934434144
Fotis Tsamis <ftsamis@gmail.com>
2020-05-27 12:30:11 +0300

    Windows build without CMAKE_GENERATOR_TOOLSET.

--- travis/build.bat
+++ travis/build.bat
@@ -1,7 +1,7 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 ::SET CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
-SET CMAKE_GENERATOR_TOOLSET="host=x64"
+SET CMAKE_GENERATOR="Visual Studio 15 2017"
+::SET CMAKE_GENERATOR_TOOLSET="host=x64"
 SET CMAKE_GENERATOR_PLATFORM="x64"
 SET CMAKE_CL_64=1
-SET CMAKE_GENERATOR="Visual Studio 15 2017"
 python setup.py bdist_wheel
