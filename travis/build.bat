c704f7ef0f366e6c25b68d912ba33166a5c5d647
Fotis Tsamis <ftsamis@gmail.com>
2020-05-27 13:02:18 +0300

    Windows build with pytest.

--- travis/build.bat
+++ travis/build.bat
@@ -5,3 +5,5 @@ SET CMAKE_GENERATOR="Visual Studio 15 2017"
 SET CMAKE_GENERATOR_PLATFORM="x64"
 SET CMAKE_CL_64=1
 python setup.py bdist_wheel
+python -m pip install python-jgrapht --no-index -f dist
+pytest
